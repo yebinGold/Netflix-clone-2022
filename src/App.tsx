3edc4b61375189759a9090b0db7be57e7cb0d4a4
Yebin Kim <rladpqls07@gmail.com>
2022-08-16 15:41:38 +0900

    file upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Home - Movies */}
+        <Route path="/tv" element={<Tv />} /> {/* TV Show */}
+        <Route path="/search" element={<Search />} /> {/* Search */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Routes/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div style={{backgroundColor: "whitesmoke", height: "200vh"}}>
+            
+        </div>
+    );
+};
+
+export default Home;
